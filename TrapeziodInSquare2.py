8b40a72eba9d508d0f416fce5f54e66fbab84bc1
bryanzhou07 <bryanzhou07@gmail.com>
2015-10-29 21:17:43 -0400

    Create TrapeziodInSquare2.py

---
+++ TrapeziodInSquare2.py
@@ -0,0 +1,16 @@
+import turtle
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+
+turtle.setpos(0,-100)
+turtle.goto(25,0)
+turtle.setheading(0)
+turtle.forward(50)
+turtle.goto(100,-100)
