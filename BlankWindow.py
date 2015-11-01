f1c202f33e457fead6f26cddcc26cf9f18ace6bb
Charlie Liu <charliematthew@hotmail.com>
2015-11-01 18:20:21 -0500

    Update BlankWindow.py

--- BlankWindow.py
+++ BlankWindow.py
@@ -2,6 +2,6 @@ import pygame
 import sys
 pygame.init()
 screen = pygame.display.set_mode((1680,1050))
-white = (255,255,255)
-screen.fill(white)
+black = (0,0,0)
+screen.fill(black)
 pygame.display.update()
