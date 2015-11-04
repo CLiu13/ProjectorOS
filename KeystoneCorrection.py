c6dc5d12645e66b6d445c10b2e7dad37e96799ce
Charlie Liu <charliematthew@hotmail.com>
2015-11-03 21:38:40 -0500

    Update and rename BlankWindow.py to KeystoneCorrection.py

--- KeystoneCorrection.py
+++ KeystoneCorrection.py
@@ -3,6 +3,8 @@ import sys
 pygame.init()
 screen = pygame.display.set_mode((1680,1050))
 black = (0,0,0)
+white = (255,255,255)
 screen.fill(black)
 pygame.display.toggle_fullscreen()
+pygame.draw.rect(screen, white, (500,500,500,500))
 pygame.display.update()
