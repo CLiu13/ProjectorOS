55cdec27899a81b87d557018038ebe1f6bcb7000
Charlie Liu <charliematthew@hotmail.com>
2015-11-09 20:16:43 -0500

    Update KeystoneCorrection.py

--- KeystoneCorrection.py
+++ KeystoneCorrection.py
@@ -8,16 +8,3 @@ screen.fill(black)
 pygame.display.toggle_fullscreen()
 pygame.draw.rect(screen, white, (500,500,500,500))
 pygame.display.update()
-done = False
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                done = True
-                break
-        elif event.type == pygame.QUIT:
-            done = True
-            break
-    if done:
-      break
-
