70cd9403efdefc2bacc65331eee4ec07ac227709
Charlie Liu <charliematthew@hotmail.com>
2015-11-09 20:17:48 -0500

    Delete PercentInput.py

--- PercentInput.py
+++
@@ -1,8 +0,0 @@
-raw=input("Please enter a percentage here (Omit %): ")
-percent=int(raw)
-if percent >= 100:
-    print("\n")    
-    print("Please enter a percentage less than 100%")
-else:
-    print("\n")
-    print("You entered", percent, "%")
