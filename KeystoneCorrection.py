03085d7bea243f09fd289dc61f7e1a429ec07d19
Charlie Liu <charliematthew@hotmail.com>
2015-11-09 20:17:41 -0500

    Merged PercentInput.py with KeystoneCorrection.py

--- KeystoneCorrection.py
+++ KeystoneCorrection.py
@@ -1,3 +1,11 @@
+raw=input("Please enter a percentage here (Omit %): ")
+percent=int(raw)
+if percent >= 100:
+    print("\n")    
+    print("Please enter a percentage less than 100%")
+else:
+    print("\n")
+    print("You entered", percent, "%")
 import pygame 
 import sys
 pygame.init()
