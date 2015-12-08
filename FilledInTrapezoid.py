f7aa16517e7e47c6938de6932e0a9736928f86c0
Charlie Liu <charliematthew@hotmail.com>
2015-12-07 21:13:25 -0500

    Updated to custom variables

--- FilledInTrapezoid.py
+++ FilledInTrapezoid.py
@@ -10,35 +10,31 @@ blue = (0,0,255)
 green = (0,255,0)
 red = (255,0,0)
 
+#Create our variables
+b1 = input("Please enter the 1st base height: ")
+b2 = input("Please enter the 2nd base height: ")
+h = input("Please enter the height: ")
+
 # Set the height and width of the screen
-size = [1000, 600]
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode((1680,1050))
 
 pygame.display.set_caption("Trapezoid Function")
 
-#Loops until the use clicks the close button
-done = False
-clock = pygame.time.Clock()
-
 # Clear the screen and set the screen background
-screen.fill(white)
+surface.fill(white)
 
-"""pygame.draw.line(screen, green, [200,200],[500,200], 5)
-pygame.draw.line(screen, green, [200,200],[300,300], 5)
-pygame.draw.line(screen, green, [300,300],[400,300], 5)
-pygame.draw.line(screen, green, [400,300],[500,200], 5)"""
-#Just keeping the above lines if we still need it
+#Translate input to coordinates
+xp1 = 700-(int(b2))/2
+yp1 = 700
+xp2 = 700+(int(b2))/2
+yp2 = 700
+xp3 = 700-(int(b1))/2
+yp3 = 700+int(h)
+xp4 = 700+(int(b1))/2
+yp4 = 700+int(h)
 
-pygame.draw.rect(screen, black, [400, 200, 200, 200])
-
-#pygame.draw.rect(screen, black, [how far across from the left, how far from
-#the top, how wide, how long])
-
-pygame.draw.polygon(screen, black, [[400,200], [200,200], [400,400]])
-pygame.draw.polygon(screen, black, [[600,200], [800,200], [600,400]])
+#Draw trapezoid FILLED IN
+pygame.draw.polygon(screen, green, [xp1,yp1], [xp2,yp2], [xp3,yp3], [xp4,yp4]) #Display, color, coordinates
 
 #Updates the screen
 pygame.display.update()
-
-pygame.quit()
-quit()
