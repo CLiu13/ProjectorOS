b5c83cd7198d35ed0b222c729c2a611a4c7d1a58
Charlie Liu <charliematthew@hotmail.com>
2015-12-07 21:10:04 -0500

    Added custom variables

--- PygameTrapezoidFunction.py
+++ PygameTrapezoidFunction.py
@@ -10,19 +10,34 @@ blue = (0,0,255)
 green = (0,255,0)
 red = (255,0,0)
 
+#Create our variables
+b1 = input("Please enter the 1st base height: ")
+b2 = input("Please enter the 2nd base height: ")
+h = input("Please enter the height: ")
+
 # Set the height and width of the screen
-size = [1000, 1000]
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode((1680,1050))
 
 pygame.display.set_caption("Trapezoid Function")
 
 # Clear the screen and set the screen background
 surface.fill(white)
 
-pygame.draw.line(screen, green, [200,200],[500,200], 5)
-pygame.draw.line(screen, green, [200,200],[300,300], 5)
-pygame.draw.line(screen, green, [300,300],[400,300], 5)
-pygame.draw.line(screen, green, [400,300],[500,200], 5)
+#Translate input to coordinates
+xp1 = 700-(int(b2))/2
+yp1 = 700
+xp2 = 700+(int(b2))/2
+yp2 = 700
+xp3 = 700-(int(b1))/2
+yp3 = 700+int(h)
+xp4 = 700+(int(b1))/2
+yp4 = 700+int(h)
+
+#Draw trapezoid
+pygame.draw.line(screen, green, [xp1,yp1],[xp2,yp2], 5) #Display, color, 1st coord, 2nd coord, width
+pygame.draw.line(screen, green, [xp2,yp2],[xp4,yp4], 5)
+pygame.draw.line(screen, green, [xp4,yp4],[xp3,yp3], 5)
+pygame.draw.line(screen, green, [xp3,yp3],[xp1,yp1], 5)
 
 #Updates the screen
 pygame.display.update()
