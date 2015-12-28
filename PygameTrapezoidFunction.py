48a06f7a70408ef7a196df845668eb4f7dba4033
Charlie Liu <charliematthew@hotmail.com>
2015-12-28 18:19:17 -0500

    Filled in now

--- PygameTrapezoidFunction.py
+++ PygameTrapezoidFunction.py
@@ -17,8 +17,8 @@ width = screenres.width
 height = screenres.height
 
 #Create our sizes
-b1 = int(width/3)
-b2 = int(width*2/3)
+b1 = int(width*2/3)
+b2 = int(width/3)
 h = int(height/3)
 
 #Create our center
@@ -31,7 +31,7 @@ screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("Trapezoid Function")
 
 # Clear the screen and set the screen background
-screen.fill(white)
+screen.fill(black)
 
 #Create our coordinates
 xp1 = int(centx - b1/2) 
@@ -43,13 +43,11 @@ yp3 = int(centy - h/2)
 xp4 = int(centx + b2/2)
 yp4 = int(centy - h/2)
 
-#Draw trapezoid OUTLINE
-pygame.draw.line(screen, green, [xp1,yp1],[xp2,yp2], 5) #Display, color, 1st coord, 2nd coord, width
-pygame.draw.line(screen, green, [xp2,yp2],[xp4,yp4], 5)
-pygame.draw.line(screen, green, [xp4,yp4],[xp3,yp3], 5)
-pygame.draw.line(screen, green, [xp3,yp3],[xp1,yp1], 5)
+#Draw trapezoid filled in
+pygame.draw.polygon(screen, green, [(xp1,yp1),(xp2,yp2),(xp4,yp4),(xp3,yp3)], 0)
 
 #Updates the screen
 pygame.display.update()
 
-time.sleep(30)
+#Freeze the screen for 10 seconds
+time.sleep(10)
